0104a18e7d8726710ee9ff5178c46c4986dad434
Calin Ursaciuc <calinu_4u@yahoo.com>
2017-07-19 11:15:33 +0000

    initial implementation of cypher

--- src/main/scala/Cypher.scala
+++ src/main/scala/Cypher.scala
@@ -1,6 +1,17 @@
-/**
-  * Created by Administrator on 19/07/2017.
-  */
-class Cypher {
 
+object Cypher {
+val cypher=Map('a'->'z','b'->'y','c'->'x','d'->'w','e'->'v','f'->'u','g'->'t','h'->'s','i'->'r','j'->'q','k'->'p','l'->'o','m'->'n','n'->'m','o'->'l','p'->'k','q'->'j','r'->'i','s'->'h','t'->'g','u'->'f','v'->'e','w'->'d','x'->'c','y'->'b','z'->'a')
+
+
+  def main(args: Array[String]): Unit = {
+
+    println(cypher)
+   println(Cypher.encodeDecode("foobar"))
+    println(Cypher.encodeDecode("wizard"))
+    println(Cypher.encodeDecode("gsrh rh zm vcznkov lu gsv zgyzhs xrksvi"))
+  }
+
+  def encodeDecode(input:String):String={
+     input.map(c=>cypher.getOrElse(c,c))
+  }
 }
